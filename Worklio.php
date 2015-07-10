b0d1dead4802341df573abd9f4e8faad3a61be4b
Matias Camiletti <matias.camiletti@gmail.com>
2015-07-09 22:05:28 -0300

    Create basic library.

---
+++ Worklio.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Description of Worklio
+ *
+ * @author matiascamiletti
+ */
+class Worklio 
+{
+    static $BASE_URL = 'http://localhost:8888/worklio-web/public/';
+    
+    private $_apiKey = '';
+    
+    public function __construct($api_key)
+    {
+        $this->_apiKey = $api_key;
+    }
+    
+    public function render($view)
+    {
+        $url = self::$BASE_URL . 'api/view/render?api_key=' . $this->_apiKey .'&view=' . $view;
+        
+        $data = file_get_contents($url);
+        $json = json_decode($data, true);
+        
+        if($json['success'] !== true){
+            return '';
+        }
+        
+        return $json['response']['render'];
+    }
+    
+}
